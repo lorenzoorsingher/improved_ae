25ab8710aa179330a4dc2db680b184942f2d152e
lollozip <lorenzo.orsingher@gmail.com>
2023-07-06 15:59:23 +0200

    make model BIGGER

--- model_files/model.py
+++ model_files/model.py
@@ -13,58 +13,62 @@ class SimplerAE2(nn.Module):
         self.conv1 = nn.Sequential(         
             nn.Conv2d(
                 in_channels=1,              
-                out_channels=8,            
-                kernel_size=5,              
+                out_channels=16,            
+                kernel_size=3,              
                 stride=2,                   
                 padding=0,                  
             ),                              
             nn.ReLU(),
-            nn.BatchNorm2d(8)                      
-        )# out [BS, 8, 38, 38]
+            nn.BatchNorm2d(16)                      
+        )# out [BS, 16, 39, 39]
 
-        # in [BS, 8, 38, 38]
+        # in [BS, 16, 39, 39]
         self.conv2 = nn.Sequential(         
             nn.Conv2d(
-                in_channels=8,              
-                out_channels=16,            
-                kernel_size=5,              
-                stride=3,                   
+                in_channels=16,              
+                out_channels=32,            
+                kernel_size=3,              
+                stride=2,                   
                 padding=0                  
             ),     
             nn.ReLU(),
-            nn.BatchNorm2d(16)                        
-        )# out [BS, 16, 12, 12]
+            nn.BatchNorm2d(32)                        
+        )# out [BS, 32, 19, 19]
         
-        # in [BS, 16, 12, 12]
+        # in [BS, 32, 19, 19]
         self.flat = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(2304, 512),
+            nn.Linear(11552, 2048),
             nn.ReLU(),
-            nn.Linear(512, 2304),
+            nn.Linear(2048, 512),
+            nn.ReLU(),
+            nn.Linear(512, 2048),
+            nn.ReLU(),
+            nn.Linear(2048, 11552),
             nn.ReLU()
-        )# out [BS, 2304]
+        )# out [BS, 11552]
 
         #output_size = strides * (input_size-1) + kernel_size - 2*padding
         
-        # in [BS, 16, 12, 12]
+        # in [BS, 32, 19, 19]
         self.trans1 = nn.Sequential(
             nn.ConvTranspose2d(
-                in_channels=16,
-                out_channels=8,
-                kernel_size=5,
-                stride=3,
+                in_channels=32,
+                out_channels=16,
+                kernel_size=3,
+                stride=2,
                 padding=0
             ),
             nn.ReLU(),
-            nn.BatchNorm2d(8)  
-        )# out [BS, 8, 38, 38]
+            nn.BatchNorm2d(16)  
+        )# out [BS, 16, 39, 39]
 
-        # in [BS, 8, 38, 38]
+        # in [BS, 16, 39, 39]
         self.trans2 = nn.Sequential(
             nn.ConvTranspose2d(
-                in_channels=8,
+                in_channels=16,
                 out_channels=3,
-                kernel_size=5,
+                kernel_size=3,
                 stride=2,
                 padding=0
             ),
@@ -79,7 +83,7 @@ class SimplerAE2(nn.Module):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.flat(x)
-        x = x.view(x.size(0), 16, 12, 12)
+        x = x.view(x.size(0), 32, 19, 19)
         x = self.trans1(x) 
         output = self.trans2(x)
         #output = self.tanh(x)
